1a243b29518f4941a820515ed285759db46a8e60
Mohamed Rojbeni <rojbenimohamed@gmail.com>
2026-06-19 23:47:31 +0200

    edit metadata

--- apps/storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -46,13 +46,11 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   const params = await props.params
   try {
     const productCategory = await getCategoryByHandle(params.category)
-
-    const title = productCategory.name + " | Medusa Store"
-
+    const title = productCategory.name + " | Aceline Store"
     const description = productCategory.description ?? `${title} category.`
 
     return {
-      title: `${title} | Medusa Store`,
+      title: `${title} | Aceline Store`,
       description,
       alternates: {
         canonical: `${params.category.join("/")}`,
