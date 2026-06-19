ac6d479dd0340c11c4f151f1153bf0e7a2148026
Mohamed Rojbeni <rojbenimohamed@gmail.com>
2026-06-19 19:40:36 +0200

    Update index.tsx

--- apps/storefront/src/modules/home/components/hero/index.tsx
+++ apps/storefront/src/modules/home/components/hero/index.tsx
@@ -9,12 +9,12 @@ const Hero = () => {
       style={{ background: "radial-gradient(circle at top right, rgba(0,0,0,0.02) 0%, transparent 70%)" }}
     >
       {/* Background Image from Aceline Store Dark Mode Home */}
-      {/* <div className="absolute inset-0 z-0 opacity-20 pointer-events-none">
+      {<div className="absolute inset-0 z-0 opacity-20 pointer-events-none">
         <div
           className="w-full h-full bg-cover bg-center"
           style={{ backgroundImage: "url('https://lh3.googleusercontent.com/aida-public/AB6AXuBwlbTRHVGB93sfUc6EmfFqEcuASM7sFsBRPUU6bygIs18hTwo9PMsW7gsq8lvWettPP1nO6CehwIiTJwZ7giZevBzbczbA95v4uq8I-8n1oM1t-k3cYFKiVPAsVGcY-_SOJO-gfz15bJjLT_NEasRwGgwXJoL8RDHukLrnuXbaIUBorjJM-OduIwoLk20X_0qqTLbpAzQsJmd0Xz1RIjO3lBA4JX3C-2u5ZlB61XdXT5yZKYUCqYtxGmgaEPLTDUq90FN8ZsmFjdc')" }}
         />
-      </div> */}
+      </div>}
 
       <div className="max-w-[1440px] mx-auto px-6 md:px-16 text-center relative z-10 w-full">
         <h1 className="font-sans text-4xl md:text-6xl font-bold max-w-4xl mx-auto mb-8 text-primary-container leading-tight drop-shadow-[0_0_15px_rgba(195,244,0,0.2)]">
